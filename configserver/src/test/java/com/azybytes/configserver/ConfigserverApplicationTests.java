b07086f7d89e12a438da704c9f1d854fba1b438c
Gianfranco <gfman@inwind.it>
2026-01-23 14:42:01 +0100

    Added Spring Cloud Config project, enabled to manage externalized configuration for microservices, defined the port where it is exposed.

---
+++ configserver/src/test/java/com/azybytes/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azybytes.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
