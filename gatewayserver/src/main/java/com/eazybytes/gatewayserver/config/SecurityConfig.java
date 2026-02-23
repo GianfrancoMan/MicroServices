0f3859c4578912b419d5cf8c2678cdfd2b995a53
Gianfranco <gfman@inwind.it>
2026-02-23 17:29:53 +0100

    Made changes to authorize requests based on roles

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.eazybytes.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,16 +19,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity.authorizeExchange(
-                        exchange -> exchange
-                                .pathMatchers(HttpMethod.GET).permitAll() //Autorizza chiunque a eseguire richieste GET su qualunque endpoint
-                                //proteggo gli endpoint accounts, cards e loans eccetto che per le GET perché il primo pathmatcher  ha priorità sui successivi.
-                                .pathMatchers("/eazybank/accounts/**").authenticated()
-                                .pathMatchers("/eazybank/loans/**").authenticated()
-                                .pathMatchers("/eazybank/cards/**").authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+        serverHttpSecurity
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET).permitAll() //Autorizza chiunque a eseguire richieste GET su qualunque endpoint
+                            //proteggo gli endpoint accounts, cards e loans eccetto che per le GET perché il primo pathmatcher  ha priorità sui successivi.
+                            .pathMatchers("/eazybank/accounts/**").hasRole ("ACCOUNTS")
+                            .pathMatchers("/eazybank/loans/**").hasRole("LOANS")
+                            .pathMatchers("/eazybank/cards/**").hasRole("CARDS"))
+                .oauth2ResourceServer(oAuth2ResourceServerSpec ->
+                        oAuth2ResourceServerSpec.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthorityExtractor())));
+
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
 
         return serverHttpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthorityExtractor() {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloackRoleConverter());
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
